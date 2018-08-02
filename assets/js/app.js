7bb9281a34098d8063c17339c4e22b5a570c95fe
Lucas Reis <lucas@programmer.com.br>
2018-08-02 01:10:06 -0300

    Add some code to example

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Explorer from '../components/Explorer'
+import File from "./file"
+import Option from './option'
+
+Vue.component('v-explorer', Explorer);
+
+new Vue({
+    el: '#app',
+    data: {
+        files: [
+            new File(1, "File 1 wqewq weqeq www wwww wwww", 0),
+            new File(2, "File 2 qwew eeeee", 5),
+            new File(3, "File 3", 10)
+        ],
+        options: [
+            new Option('Delete', (e) => {
+            }, (file) => {
+                return file.id == 2;
+            }),
+            new Option('Save', (e) => {
+            })
+        ]
+    },
+    methods: {
+        contextmenu(e) {
+            console.log(e);
+        }
+    }
+});

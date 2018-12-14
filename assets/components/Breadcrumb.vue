1fe73aae6f88e2b3c6cd27b937f9f742d1200ad2
Lucas Reis <lucas@programmer.com.br>
2018-12-13 23:20:37 -0300

    Add a breadcrumb
    Added component to show a breadcrumb at the top of Explorer (under dev)

---
+++ assets/components/Breadcrumb.vue
@@ -0,0 +1,33 @@
+<template>
+    <transition-group name="v-exp-list" tag="span">
+        <span v-for="item in path" :key="item.name">{{item.name}}</span>
+    </transition-group>
+</template>
+<script>
+
+export default {
+    props: {
+        path: {
+            required: true,
+            type: Array
+        },
+    },
+    mounted(){
+    },
+    methods: {
+    }
+}
+
+</script>
+<style lang="stylus" scoped>
+
+    .v-exp-list-enter, .v-exp-list-leave-to {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+
+    .v-exp-list-leave-active {
+        position: absolute;
+    }
+
+</style>
---
+++ assets/js/breadcrumb.js
@@ -0,0 +1,10 @@
+/**
+ * BreadcrumbItem class
+ * @author Lucas Reis <lucas@programmer.com.br>
+ */
+export default class BreadcrumbItem {
+    constructor(name, folder) {
+        this.name = name;
+        this.folder = folder;
+    }
+}

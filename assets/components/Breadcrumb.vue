1d23ef0605c2ed1a4e9b16fe07326318bf375781
Lucas.Reis <lucas.reis@topos.local>
2018-12-14 17:01:39 -0300

    Adição de css de breadcrumb

--- assets/components/Breadcrumb.vue
+++ assets/components/Breadcrumb.vue
@@ -1,7 +1,9 @@
 <template>
-    <transition-group name="v-exp-list" tag="span">
-        <span v-for="item in path" :key="item.name">{{item.name}}</span>
-    </transition-group>
+	<div class="v-exp-breadcrumb">
+		<transition-group name="v-exp-list" tag="span">
+			<a href="javascript:void(0)" v-for="item in path" :key="item.name">{{item.name}}</a>
+		</transition-group>
+	</div>
 </template>
 <script>
 
@@ -21,13 +23,29 @@ export default {
 </script>
 <style lang="stylus" scoped>
 
+	.v-exp-breadcrumb {
+		display block !important
+		height 15px !important
+		width 100% !important
+		position relative
+		top -16px
+		a {
+			text-decoration none
+			color gray
+			cursor pointer
+			&:hover{
+				opacity .65
+			}
+		}
+	}
+
     .v-exp-list-enter, .v-exp-list-leave-to {
-        opacity: 0;
-        transform: translateY(30px);
+        opacity 0
+        transform translateY(30px)
     }
 
     .v-exp-list-leave-active {
-        position: absolute;
+        position absolute
     }
 
 </style>
